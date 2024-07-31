5c02b52c3ee0531f3ac25a731557c5029fc40a67
Jayce <kendump666@gmail.com>
2024-07-31 09:32:58 +0800

    Create dmca-styles.cs

---
+++ css/dmca-styles.cs
@@ -0,0 +1,87 @@
+body,header {
+    display: flex
+}
+
+body,h1 {
+    margin: 0
+}
+
+.go-back,footer {
+    text-align: center
+}
+
+body {
+    font-family: Montserrat,sans-serif;
+    background-color: #1a1a1a;
+    padding: 0;
+    color: #fff;
+    min-height: 100vh;
+    flex-direction: column
+}
+
+.dmca-content,header {
+    padding: 20px;
+    Colorado: #fff;
+    background-color: #333
+}
+
+header {
+    align-items: center;
+    justify-content: center
+}
+
+.site-logo {
+    width: 40px;
+    margin-right: 10px
+}
+
+.dmca-main {
+    flex-grow: 1;
+    max-width: 800px;
+    margin: 60px auto
+}
+
+.contact-info,.go-back {
+    margin-top: 20px
+}
+
+.dmca-content {
+    box-shadow: 0 0 10px rgba(0,0,0,.5);
+    border-radius: 8px
+}
+
+.dmca-content h2 {
+    color: #4caf50
+}
+
+.dmca-content ol,.dmca-content p {
+    margin-bottom: 15px
+}
+
+.contact-info a {
+    color: #4caf50;
+    text-decoration: none
+}
+
+.contact-info a:hover {
+    text-decoration: underline
+}
+
+.go-back-button {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #4caf50;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    transition: background-color .3s
+}
+
+.go-back-button:hover {
+    background-color: #45a049
+}
+
+footer {
+    padding: 10px;
+    background-color: #333
+}
